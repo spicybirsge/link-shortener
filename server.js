114327aa5844f06b9f502d3e059c1fb04aa55f36
Shaheer Ahamed <92243459+spicybirsge@users.noreply.github.com>
2023-05-22 11:26:18 +0530

    *port

--- server.js
+++ server.js
@@ -74,6 +74,6 @@ app.get('/', (req, res) => {
    
   })
   
-  app.listen(process.env.POST || 5000, () => { 
+  app.listen(process.env.PORT || 5000, () => { 
       console.log('App Started!') 
     });
